d14e9d27d7e1d23b19f3e3bc528d3ecdbb6fdf47
Assembly <81379378+tekq@users.noreply.github.com>
2026-02-05 23:59:00 +0200

    Refactor cachy-nvidia.sh to streamline installations
    Removed unnecessary kernel and akmods installation steps, and adjusted package installation commands.

--- files/scripts/cachy-nvidia.sh
+++ files/scripts/cachy-nvidia.sh
@@ -14,24 +14,28 @@ dnf -y copr enable bieszczaders/kernel-cachyos-addons
 dnf -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-multimedia.repo || true
 dnf -y config-manager addrepo --from-repofile=https://raw.githubusercontent.com/terrapkg/subatomic-repos/main/terra.repo || true
 
-dnf -y install --setopt=install_weak_deps=False akmods
-
-dnf -y swap kernel kernel-cachyos-lto
-
-dnf -y swap kernel-devel kernel-cachyos-lto-devel
-
-dnf -y swap zram-generator-defaults cachyos-settings
-
-
 dnf -y install --setopt=install_weak_deps=False \
+    kernel-cachyos-lto \
+    kernel-cachyos-lto-devel \
+    kernel-cachyos-lto-core \
+    kernel-cachyos-lto-modules \
+    kernel-cachyos-lto-nvidia-open \
+    nvidia-driver-libs \
+    nvidia-settings \
     scx-scheds \
     scx-tools \
     scx-manager
-
+    
+dnf -y swap zram-generator-defaults cachyos-settings
 
 VER=$(ls /lib/modules) && \
     akmods --force --kernels $VER --kmod nvidia && \
     depmod -a $VER && \
     dracut --kver $VER --force --add ostree --no-hostonly --reproducible /usr/lib/modules/$VER/initramfs.img
+
+
+dnf -y remove \
+    kernel
+    kernel-devel
     
 rm -f /etc/yum.repos.d/{*copr*,*multimedia*,*terra*}.repo
