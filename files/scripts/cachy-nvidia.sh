69a012beaa551407a1fc821d334c9e88816256a8
Assembly <81379378+tekq@users.noreply.github.com>
2026-02-06 20:00:00 +0200

    Add files via upload

--- files/scripts/cachy-nvidia.sh
+++ files/scripts/cachy-nvidia.sh
@@ -6,27 +6,23 @@ dnf -y copr enable bieszczaders/kernel-cachyos-lto
 dnf -y copr enable bieszczaders/kernel-cachyos-addons
 
 ln -sf /bin/true /usr/bin/kernel-install
+ln -sf /bin/true /usr/bin/dracut
 
-dnf -y swap kernel kernel-cachyos-lto \
-    --allowerasing \
-    --setopt=install_weak_deps=False
-
+dnf -y swap kernel kernel-cachyos-lto --allowerasing --setopt=install_weak_deps=False
 dnf -y install \
     kernel-cachyos-lto-core \
     kernel-cachyos-lto-modules \
     kernel-cachyos-lto-nvidia-open \
-    kernel-cachyos-lto-devel-matched \
     nvidia-driver \
     nvidia-driver-libs \
-    nvidia-settings \
-    nvidia-driver-cuda
+    nvidia-driver-cuda \
+    nvidia-settings
 
 KVER=$(rpm -q kernel-cachyos-lto-core --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')
 depmod -a "$KVER"
 
-find /usr/lib/modules -maxdepth 1 -type d -name "6.*.fc43.x86_64" ! -name "*cachyos*" -exec rm -rf {} +
+find /usr/lib/modules -maxdepth 1 -mindepth 1 -type d ! -name "*cachyos*" -exec rm -rf {} +
+
+rm -f /usr/bin/kernel-install /usr/bin/dracut
 
 dnf -y install scx-scheds scx-tools
-
-rm -f /usr/bin/kernel-install
-rm -f /etc/yum.repos.d/{*copr*,*multimedia*,*terra*}.repo
