0a90b41240a69caf4ce5060c707b0c505cad083d
Assembly <81379378+tekq@users.noreply.github.com>
2026-02-06 17:41:06 +0200

    Replace rpm-ostree command with dnf install

--- files/scripts/cachy-nvidia.sh
+++ files/scripts/cachy-nvidia.sh
@@ -9,6 +9,6 @@ dnf -y install --setopt=install_weak_deps=False \
 dnf -y copr enable bieszczaders/kernel-cachyos-lto
 dnf -y copr enable bieszczaders/kernel-cachyos-addons
 
-rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos-lto --install kernel-cachyos-lto-core --install kernel-cachyos-lto-modules --install kernel-cachyos-lto-devel --install kernel-cachyos-lto-devel-matched
+dnf -y install kernel-cachyos-lto kernel-cachyos-lto-devel-matched
 
 rm -f /etc/yum.repos.d/{*copr*,*multimedia*,*terra*}.repo
